6889f392a18c0e3e09343b0581760e293c887aba
James Newman <jamesmnewman1@gmail.com>
2014-11-15 01:21:04 -0800

    live

--- app.rb
+++ app.rb
@@ -15,3 +15,11 @@ require 'pony'
 get "/" do 
   erb :"welcome/index"
 end
+
+get '/javascripts/:main.js' do |script|
+  javascript :"javascripts/#{script}"
+end
+
+get '/stylesheets/:styles.css' do |stylesheet|
+  css :"stylesheets/#{stylesheet}"
+end
