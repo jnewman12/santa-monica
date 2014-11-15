036d4b6dc333e3475ad35dd45122839ce01787b5
James Newman <jamesmnewman1@gmail.com>
2014-11-14 19:47:49 -0800

    working on localhost

--- app.rb
+++ app.rb
@@ -1,8 +1,17 @@
 # file that is the main root of the site
 # this is used for everything from displaying routes, to redirecting, get/post requests, etc. 
 
+# in development, see the app on http://localhost:4567/
+
+# the require 'bundler/setup' before the other gems is because of this http://stackoverflow.com/questions/11110148/rack-sinatra-loaderror-cannot-load-such-file
+
+require 'bundler/setup'
 require 'sinatra'
 #require 'sinatra/activerecord'
 #require './environments'
+require 'pony'
 
 
+get "/" do 
+  erb :"welcome/index"
+end
